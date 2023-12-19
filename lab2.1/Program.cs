7ade46083b65e57a882bd8464e9dff5ad483ff08
KRISgardian <marmanplay9@gmail.com>
2023-12-19 23:09:14 +0200

    new file:   lab2.1/Program.cs 	new file:   lab2.1/bin/Debug/net8.0/lab2.1.exe

---
+++ lab2.1/Program.cs
@@ -0,0 +1,34 @@
+﻿Console.Write("Время разговоров в минутах: ");
+int dt = 0; dt = int.Parse(Console.ReadLine()); //время разговора в минутах
+Console.Write("Введите страну для переговоров: ");
+string country = Console.ReadLine(); //страна переговоров
+Console.Write("Введите день недели: ");
+string d = Console.ReadLine(); //день недели
+double price = 0; //общая цена 
+double s = 0; //цена за минуту
+if (country.ToLower() == "россия" && (d.ToLower() == "суббота" || d == "воскресенье"))
+{
+    s = 0.95; //в россии цена 0.95
+    price = s * dt; //цена без скидок
+    price = price / 100 * 90; //цена со скидкой
+    Console.WriteLine($"Цена в Росии со скидкой: {price:F2}");
+}
+else if (country.ToLower() == "россия" && (d.ToLower() != "суббота" || d != "воскресенье"))
+{
+    s = 0.95; //в россии цена 0.95
+    price = s * dt;
+    Console.WriteLine($"Цена в России без скидок: {price:F2}");
+}
+if (country.ToLower() == "украина" && (d.ToLower() == "суббота" || d == "воскресенье"))
+{
+    s = 0.35; //в украине цена 0.35
+    price = s * dt; //цена без скидок
+    price = price / 100 * 90; //цена со скидкой
+    Console.WriteLine($"Цена на Украине со скидкой: {price:F2}");
+}
+else if (country.ToLower() == "украина" && (d.ToLower() != "суббота" || d != "воскресенье"))
+{
+    s = 0.35; //в украине цена 0.35
+    price = s * dt;
+    Console.WriteLine($"Цена на Украине без скидок: {price:F2}");
+}
