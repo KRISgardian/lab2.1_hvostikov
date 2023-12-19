4fc00d78b889a92517b7043a7b2dfe920ed27fc6
KRISgardian <marmanplay9@gmail.com>
2023-12-20 00:01:16 +0200

    new file:   lab2.2/Program.cs 	new file:   lab2.2/bin/Debug/net8.0/lab2.2.deps.json 	new file:   lab2.2/bin/Debug/net8.0/lab2.2.dll 	new file:   lab2.2/bin/Debug/net8.0/lab2.2.exe 	new file:   lab2.2/bin/Debug/net8.0/lab2.2.pdb 	new file:   lab2.2/bin/Debug/net8.0/lab2.2.runtimeconfig.json

---
+++ lab2.2/Program.cs
@@ -0,0 +1,15 @@
+﻿Console.Write("Введите трехзначное число: ");
+int b = int.Parse(Console.ReadLine());//введенное число
+Console.Write("Введите число b: ");
+int n = int.Parse(Console.ReadLine());//число b
+int b3 = b % 10;//третья цифра
+int b2 = ((b % 100) - b3) / 10;//вторая цифра
+int b1 = (b - (b2 + b3)) / 100;//первая цифра
+int v = b1 * b2 * b3;//произведение цифр числа
+Console.WriteLine($"{v}");
+if (v > n) { Console.WriteLine("Произведение цифр больше b"); }
+else if (v == n) { Console.WriteLine("Произведение цифр равно b"); }
+else { Console.WriteLine("Произведение цифр меньше b"); }
+v = (b1 + b2 + b3) % 3;
+if (v != 0) { Console.WriteLine("Сумма цифр кратна 3"); }
+else { Console.WriteLine("Сумма цифр не кратна 3"); }
