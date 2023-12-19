b5fc9cea5d4a3a56c945db54a0feff4f6787dbe9
KRISgardian <marmanplay9@gmail.com>
2023-12-20 00:05:53 +0200

    modified:   lab2.2/Program.cs 	modified:   lab2.2/bin/Debug/net8.0/lab2.2.dll 	modified:   lab2.2/bin/Debug/net8.0/lab2.2.exe 	modified:   lab2.2/bin/Debug/net8.0/lab2.2.pdb

--- lab2.2/Program.cs
+++ lab2.2/Program.cs
@@ -6,10 +6,9 @@ int b3 = b % 10;//третья цифра
 int b2 = ((b % 100) - b3) / 10;//вторая цифра
 int b1 = (b - (b2 + b3)) / 100;//первая цифра
 int v = b1 * b2 * b3;//произведение цифр числа
-Console.WriteLine($"{v}");
 if (v > n) { Console.WriteLine("Произведение цифр больше b"); }
 else if (v == n) { Console.WriteLine("Произведение цифр равно b"); }
 else { Console.WriteLine("Произведение цифр меньше b"); }
 v = (b1 + b2 + b3) % 3;
-if (v != 0) { Console.WriteLine("Сумма цифр кратна 3"); }
-else { Console.WriteLine("Сумма цифр не кратна 3"); }
+if (v != 0) { Console.WriteLine("Сумма цифр кратна 3"); Console.ReadKey(); }
+else { Console.WriteLine("Сумма цифр не кратна 3"); Console.ReadKey(); }
