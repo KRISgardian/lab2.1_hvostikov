0a3d4c102276ca4a91b34e0132a5800b8ec78c77
KRISgardian <marmanplay9@gmail.com>
2023-12-20 21:16:05 +0200

    new file:   lab2.3/Program.cs 	new file:   lab2.3/bin/Debug/net8.0/lab2.3.deps.json 	new file:   lab2.3/bin/Debug/net8.0/lab2.3.dll 	new file:   lab2.3/bin/Debug/net8.0/lab2.3.exe 	new file:   lab2.3/bin/Debug/net8.0/lab2.3.pdb 	new file:   lab2.3/bin/Debug/net8.0/lab2.3.runtimeconfig.json

---
+++ lab2.3/Program.cs
@@ -0,0 +1,52 @@
+﻿using System.Diagnostics;
+using System.Security.Cryptography;
+
+Console.WriteLine("Красный зал - Хроники Нарнии\n    12ч - 25гр\n    16ч - 35гр\n    20ч - 45гр\nСиний зал - Чужие\n    10ч - 25гр\n    13ч - 35гр\n    16ч - 35гр\nГолубой зал - " +
+    "Аватар\n    10ч - 35гр\n    14ч - 45гр\n    18ч - 45гр");
+Console.Write("Введите название зала: "); string hall = Console.ReadLine().ToLower();
+int time = 0;//время сеанса
+double price = 0;//переменная для итоговой цены
+int tickets = 0;//колличество билетов
+//выясняем какой зал, дефолтом ловим ошибку ввода зала
+
+if (hall == "красный зал") {//красный зал
+    Console.Write("Введите время сеанса: "); time = int.Parse(Console.ReadLine());//узнаем время сеанса 
+    if (time != 12 && time != 16 && time != 20) { Console.WriteLine("[ERROR] Время введено с ошибкой"); System.Environment.Exit(1); }//проверка на ошибки ввода времени сеанса
+    else { ; }
+    Console.Write("Введите колличество билетов: "); tickets = int.Parse(Console.ReadLine());//берем колличество билетов
+    if (time == 12) { price = tickets * 25; }//*************************
+    else if (time == 16) { price = tickets * 35; }//вычисляем цену до скидок за колличество билетов
+    else { price = tickets * 45; }//*************************
+    if (tickets >= 5 && tickets < 10) { price = (price / 100) * 95; }//*************************
+    else if (tickets >= 10) { price = (price / 100) * 90; }//вычисление скидки для билетов
+    Console.WriteLine(price);
+} //конец красного зал
+
+else if (hall == "синий зал")
+{//синий зал
+    Console.Write("Введите время сеанса: "); time = int.Parse(Console.ReadLine());//узнаем время сеанса
+    if (time != 10 && time != 13 && time != 16) { Console.WriteLine("[ERROR] Время введено с ошибкой"); System.Environment.Exit(1); }//проверка на ошибки ввода времени сеанса
+    Console.Write("Введите колличество билетов: "); tickets = int.Parse(Console.ReadLine());//берем колличество билетов
+    if (time == 10) { price = tickets * 25; }//*************************
+    else if (time == 13) { price = tickets * 35; }//вычисляем цену до скидок за колличество билетов
+    else { price = tickets * 35; }//*************************
+    if (tickets >= 5 && tickets < 10) { price = (price / 100) * 95; }//*************************
+    else if (tickets >= 10) { price = (price / 100) * 90; }//вычисление скидки для билетов
+    else { price = price; }//*************************
+    Console.WriteLine(price);
+}//конец синего зала
+
+else if (hall == "голубой зал")
+{//голубой зал
+    Console.Write("Введите время сеанса: "); time = int.Parse(Console.ReadLine());//узнаем время сеанса
+    if (time != 10 && time != 14 && time != 18) { Console.WriteLine("[ERROR] Время введено с ошибкой"); System.Environment.Exit(1); }//проверка на ошибки ввода времени сеанса
+    Console.Write("Введите колличество билетов: "); tickets = int.Parse(Console.ReadLine());//берем колличество билетов
+    if (time == 10) { price = tickets * 35; }//*************************
+    else if (time == 14) { price = tickets * 45; }//вычисляем цену до скидок за колличество билетов
+    else { price = tickets * 45; }//*************************
+    if (tickets >= 5 && tickets < 10) { price = (price / 100) * 95; }//*************************
+    else if (tickets >= 10) { price = (price / 100) * 90; }//вычисление скидки для билетов
+    else { price = price; }//*************************
+    Console.WriteLine(price);
+}//конец голубого зала
+else { System.Environment.Exit(1); }
