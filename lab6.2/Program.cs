0b8290863fbc9b120ef6e64564e14aa67db74614
KRISgardian <marmanplay9@gmail.com>
2023-12-21 16:31:48 +0200

    new file:   lab6.2/Program.cs 	new file:   lab6.2/bin/Debug/net8.0/lab6.2.deps.json 	new file:   lab6.2/bin/Debug/net8.0/lab6.2.dll 	new file:   lab6.2/bin/Debug/net8.0/lab6.2.exe 	new file:   lab6.2/bin/Debug/net8.0/lab6.2.pdb 	new file:   lab6.2/bin/Debug/net8.0/lab6.2.runtimeconfig.json 	new file:   lab6.2/bin/Debug/net8.0/lab6.2_dll.dll 	new file:   lab6.2/bin/Debug/net8.0/lab6.2_dll.pdb 	new file:   lab6.2_dll/Program.cs 	new file:   lab6.2_dll/bin/Debug/net8.0/lab6.2_dll.deps.json 	new file:   lab6.2_dll/bin/Debug/net8.0/lab6.2_dll.dll 	new file:   lab6.2_dll/bin/Debug/net8.0/lab6.2_dll.pdb

---
+++ lab6.2/Program.cs
@@ -0,0 +1,19 @@
+﻿using moyaveselayaferma;
+
+double[,]? mas = new double[7,7];//делаем массив
+Random rnd = new Random();//делаем рандом класс 
+//****************
+Console.ForegroundColor = ConsoleColor.Yellow;
+for (int i = 0; i < 7; i++) 
+    for(int j = 0; j < 7; j++)
+    {
+        mas[i,j] = rnd.Next(1,9);//наполнение массива
+        Console.Write(mas[i, j] + " "); 
+
+    }
+//****************
+Console.Write(" [INPUT_MATRIX]");
+Console.ResetColor();
+Console.WriteLine();
+Cow.corn(mas);//вызов .dll функции 
+Console.ReadKey();
---
+++ lab6.2_dll/Program.cs
@@ -0,0 +1,48 @@
+﻿namespace moyaveselayaferma;
+
+public class Cow
+{
+    public static int corn(double[,] mas)
+    {
+        double[] diag1 = new double[7];//две диагонали 
+        double[] diag2 = new double[7];//две диагонали 
+        //***************
+        Console.ForegroundColor = ConsoleColor.Red;
+        for (int i = 0; i < 7; i++)
+        {
+            diag1[i] = mas[i, i];
+              Console.WriteLine(diag1[i] + " [FIRST_DIAG]"); //вычисление значений главной диагонали
+        }
+        //***************
+        Console.WriteLine();
+        int temp1 = 0; int temp2 = 6;
+        Console.ForegroundColor = ConsoleColor.Green;
+        for (int i = 0; i < 7; i++)
+        {       
+            diag2[i] = mas[temp1, temp2];
+            temp1 = temp1 + 1; temp2 = temp2 - 1;
+            Console.WriteLine(diag2[i] + " [SECOND_DIAG]");//вычисление значений вторичной диагонали
+        }
+        //***************
+        Console.WriteLine();
+        Console.ForegroundColor = ConsoleColor.Magenta;
+        for (int i = 0; i < 7; i++)
+        {
+            mas[i,i] = diag2[i];
+            Console.WriteLine(mas[i, i] + " [NEW_FIRST_DIAG]");//заполнение главной диагонали вторичной
+        }
+        Console.ForegroundColor= ConsoleColor.Blue;
+        //***************
+        temp1 = 0; temp2 = 6;
+        Console.WriteLine();
+        for (int i = 0; i < 7; i++)
+        {
+            mas[temp1,temp2] = diag1[i];
+            Console.WriteLine(mas[temp1,temp2] + " [NEW_SECOND_DIAG]");//заполнение вторичной диагонали главной
+            temp1 = temp1 + 1; temp2 = temp2 - 1;
+        }
+        Console.ResetColor();
+        //***************
+        return 0;
+    }
+}
