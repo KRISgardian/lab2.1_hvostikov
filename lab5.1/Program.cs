1cb1f9a8e2c38761aad0b0ac5091daf02e76684f
KRISgardian <marmanplay9@gmail.com>
2023-12-22 22:25:22 +0200

    new file:   lab5.1/Program.cs 	new file:   lab5.1/bin/Debug/net8.0/lab5.1.deps.json 	new file:   lab5.1/bin/Debug/net8.0/lab5.1.dll 	new file:   lab5.1/bin/Debug/net8.0/lab5.1.exe 	new file:   lab5.1/bin/Debug/net8.0/lab5.1.pdb 	new file:   lab5.1/bin/Debug/net8.0/lab5.1.runtimeconfig.json

---
+++ lab5.1/Program.cs
@@ -0,0 +1,18 @@
+﻿using System.Globalization;
+
+
+Console.ForegroundColor = ConsoleColor.Red; Console.Write("Введите текст: "); Console.ResetColor();
+string st1 = Console.ReadLine(); 
+char[] ch1 = new char[st1.Length];
+for(int j = 0; j < st1.Length; j++)
+{
+    ch1[j] = Convert.ToChar(st1[j]);
+}
+double ctr = 0;
+for (int i = 0; i < ch1.Length; i++)
+{
+    UnicodeCategory category = UnicodeCategory.UppercaseLetter;
+    if (char.GetUnicodeCategory(ch1[i]) == category) {  ctr++; }
+}
+double result = (ctr / st1.Length) * 100;
+Console.WriteLine($"Процент заглавных букв в предложении: {result:F2}");
