4a7ef19bba702538e149f76f8624e404c7892978
KRISgardian <marmanplay9@gmail.com>
2023-12-28 01:51:05 +0200

    new file:   lab3.4/Program.cs 	new file:   lab3.4/bin/Debug/net8.0/lab3.4.deps.json 	new file:   lab3.4/bin/Debug/net8.0/lab3.4.dll 	new file:   lab3.4/bin/Debug/net8.0/lab3.4.exe 	new file:   lab3.4/bin/Debug/net8.0/lab3.4.pdb 	new file:   lab3.4/bin/Debug/net8.0/lab3.4.runtimeconfig.json

---
+++ lab3.4/Program.cs
@@ -0,0 +1,19 @@
+﻿double y = 0;
+Console.WriteLine("|    x   |   y    |");
+for (double x = -2; x <= 3; x += 0.2)
+{
+    if (x < 0)
+    {
+        
+    }
+    else if ((0 <= x) && (x <= 1))
+    {
+        y = y + 1;
+    }
+    else if (x > 1.5)
+    {
+        y = (2.5*Math.Pow(y, 3)) + (6*Math.Pow(y, 2) - 30);
+    }
+    Console.WriteLine($"|{x,8:F2}|{y,8:F2}|");
+
+}
