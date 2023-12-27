0324967eae8ed066aa497c9a2db88b1726d395d6
KRISgardian <marmanplay9@gmail.com>
2023-12-28 00:46:20 +0200

    new file:   lab3.2/Program.cs 	new file:   lab3.2/bin/Debug/net8.0/lab3.2.deps.json 	new file:   lab3.2/bin/Debug/net8.0/lab3.2.dll 	new file:   lab3.2/bin/Debug/net8.0/lab3.2.exe 	new file:   lab3.2/bin/Debug/net8.0/lab3.2.pdb 	new file:   lab3.2/bin/Debug/net8.0/lab3.2.runtimeconfig.json

---
+++ lab3.2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите минимальное число: ");
+int m = int.Parse(Console.ReadLine());
+Console.Write("Введите максимальное число: ");
+int n = int.Parse(Console.ReadLine());
+if(n < m) { Console.Write("Числа введены неправильно"); Environment.Exit(1); }
+int b = 0;
+for(; m < n+1; m++)
+{
+    b = (m*m) + b;
+}
+Console.Write(b);
