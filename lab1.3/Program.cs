be5a1db7120d78c1851438e24778c68e55497786
KRISgardian <marmanplay9@gmail.com>
2023-12-27 23:08:49 +0200

    new file:   lab1.3/Program.cs 	new file:   lab1.3/bin/Debug/net8.0/lab1.3.deps.json 	new file:   lab1.3/bin/Debug/net8.0/lab1.3.dll 	new file:   lab1.3/bin/Debug/net8.0/lab1.3.exe 	new file:   lab1.3/bin/Debug/net8.0/lab1.3.pdb 	new file:   lab1.3/bin/Debug/net8.0/lab1.3.runtimeconfig.json

---
+++ lab1.3/Program.cs
@@ -0,0 +1,9 @@
+﻿int b = 456;
+int b3 = b % 10;//третья цифра
+int b2 = ((b % 100) - b3) / 10;//вторая цифра
+int b1 = (b - (b2 + b3)) / 100;//первая цифра
+string b4 = Convert.ToString(b3);//вторая цифра числа х
+string b5 = Convert.ToString(b2);//третья цифра числа х
+string b6 = Convert.ToString(b1);//первая цифра числа ъ
+string b7 = b6 + b4 + b5;
+Console.Write(b7); Console.ReadKey();
