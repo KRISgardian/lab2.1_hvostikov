4a1d45179a48ddccceceaa553f0c92a5a4fbaf1d
KRISgardian <marmanplay9@gmail.com>
2023-12-28 01:25:27 +0200

    new file:   lab3.3/Program.cs 	new file:   lab3.3/bin/Debug/net8.0/lab3.3.deps.json 	new file:   lab3.3/bin/Debug/net8.0/lab3.3.dll 	new file:   lab3.3/bin/Debug/net8.0/lab3.3.exe 	new file:   lab3.3/bin/Debug/net8.0/lab3.3.pdb 	new file:   lab3.3/bin/Debug/net8.0/lab3.3.runtimeconfig.json

---
+++ lab3.3/Program.cs
@@ -0,0 +1,20 @@
+﻿Console.Write("Введите колличество циклов минимально 1: ");
+double b = double.Parse(Console.ReadLine());
+if(b == 1) { Console.Write("Число введено неверно"); Environment.Exit(1); }
+Console.Write("Введите x: ");
+double x = double.Parse(Console.ReadLine());
+double c = 0;
+for (int i = 0; i < b; i++ ){
+    double d = 3; double s = 2;  
+    c = Math.Acos(x);
+    if(i == 0)
+    {
+        c = c + (Math.Acos(d * x)) / (d * d);
+    }
+    else
+    {
+        d = d + s;
+        c = c + (Math.Acos(d * x)) / (d * d);
+    }
+}
+Console.Write(c);
