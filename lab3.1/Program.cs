42cd1f65ab024195ff83db96052e474deb3edebe
KRISgardian <marmanplay9@gmail.com>
2023-12-28 00:25:21 +0200

    new file:   lab3.1/Program.cs 	new file:   lab3.1/bin/Debug/net8.0/lab3.1.deps.json 	new file:   lab3.1/bin/Debug/net8.0/lab3.1.dll 	new file:   lab3.1/bin/Debug/net8.0/lab3.1.exe 	new file:   lab3.1/bin/Debug/net8.0/lab3.1.pdb 	new file:   lab3.1/bin/Debug/net8.0/lab3.1.runtimeconfig.json

---
+++ lab3.1/Program.cs
@@ -0,0 +1,12 @@
+﻿Console.Write("Введите число N>0: ");
+int N = int.Parse(Console.ReadLine());
+if (N < 0) { Console.Write("Число не может быть меньше 0"); Environment.Exit(1); }
+double x = 2; double b = 0;
+for(double i = 2; i < N+1; i++)
+{
+    if (i == 2){ b = x * (1 / i); }
+    else { b = b * (1 / i); }
+    
+}
+
+Console.Write($"x = {b}");
