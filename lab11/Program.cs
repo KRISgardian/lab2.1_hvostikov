fa4f8ddc7e94cef8716488d26b74957fd2807cf3
KRISgardian <marmanplay9@gmail.com>
2023-12-27 21:54:36 +0200

    new file:   lab11/Program.cs 	new file:   lab11/bin/Debug/net8.0/lab11.deps.json 	new file:   lab11/bin/Debug/net8.0/lab11.dll 	new file:   lab11/bin/Debug/net8.0/lab11.exe 	new file:   lab11/bin/Debug/net8.0/lab11.pdb 	new file:   lab11/bin/Debug/net8.0/lab11.runtimeconfig.json

---
+++ lab11/Program.cs
@@ -0,0 +1,38 @@
+﻿
+
+
+
+NeSportic spc = new NeSportic();
+Console.Write("Введите фамилию: ");
+spc.Familia = Console.ReadLine();
+Console.Write("Введите число соревнований:");
+spc.ChisloSorevnovaniy = int.Parse(Console.ReadLine());
+Console.Write("Введите сумму мест: ");
+spc.SummaMest = int.Parse(Console.ReadLine());
+Console.Write("Введите true если занимал первое место, если нет введите false: ");
+string b = Console.ReadLine();
+if (b == "true") { spc.PervoeMesto = true; }
+else if (b == "false") {  spc.PervoeMesto = false; }
+spc.Nsp(); Console.ReadKey();
+
+class NeSportic : Sportic
+{
+    public bool PervoeMesto { get; set; }
+    public double Qp {  get; set; }
+    public void Nsp()
+    {
+        Q = ChisloSorevnovaniy / SummaMest;
+        if (PervoeMesto == true) { Qp = 1.5 * Q; }
+        else if (PervoeMesto == false) { Qp = Q; }
+        Console.Write($"Фамилия = {Familia}\nЧисло соревнований = {ChisloSorevnovaniy}\nСумма мест = {SummaMest}\nЗанимал ли первое место = {PervoeMesto}\nQ = {Q:F2}\nQp = {Qp:F2}");
+    }
+
+}
+
+class Sportic
+{
+    public string ?Familia { get; set; }
+    public double ChisloSorevnovaniy { get; set; }
+    public double SummaMest {  get; set; }
+    public double Q {  get; set; } 
+}
