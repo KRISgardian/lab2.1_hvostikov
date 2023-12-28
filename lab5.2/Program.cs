0c876cd9273b6eaa6ddb746c48cb81d2a2b2089c
KRISgardian <marmanplay9@gmail.com>
2023-12-28 02:56:49 +0200

    new file:   lab5.2/Program.cs 	new file:   lab5.2/bin/Debug/net8.0/lab5.2.deps.json 	new file:   lab5.2/bin/Debug/net8.0/lab5.2.dll 	new file:   lab5.2/bin/Debug/net8.0/lab5.2.exe 	new file:   lab5.2/bin/Debug/net8.0/lab5.2.pdb 	new file:   lab5.2/bin/Debug/net8.0/lab5.2.runtimeconfig.json

---
+++ lab5.2/Program.cs
@@ -0,0 +1,11 @@
+﻿Console.Write("Введите строку: ");
+string input = Console.ReadLine();
+char[] charArray = input.ToCharArray();
+for (int i = 0; i < input.Length - 1; i += 2)
+{
+    char temp = charArray[i];
+    charArray[i] = charArray[i + 1];
+    charArray[i + 1] = temp;
+}
+string result = new string(charArray);
+Console.WriteLine("Результат: " + result);
